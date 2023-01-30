089fad3ef903f9e06b291cf08d0b2d6273bf8c70
FlowerSea0208 <zzp060208@gmail.com>
2023-01-30 12:03:52 +0800

    sm8350-common: Rename IFAAService to avoid naming conflict

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -880,7 +880,7 @@ PRODUCT_PACKAGES += \
     libfastcvopt \
     CACertService \
     CneApp \
-    IFAAService \
+    IFAAServiceXiaomism8350 \
     IWlanService \
     SoterService \
     TimeService \
